2ee896f65f0a569a58b6a071f0f0bf00b095963c
Gubner <90917279+Gubner@users.noreply.github.com>
2022-05-04 19:24:14 -0700

    Update client.lua
    Updated to remove crosshair when taking cover but not aiming

--- client.lua
+++ client.lua
@@ -3,7 +3,7 @@ local UseCrosshair = true
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(5)
-		if UseCrosshair and IsAimCamActive() then
+		if UseCrosshair and IsAimCamActive() and IsPlayerFreeAiming(PlayerId()) then
 			DrawRect(0.5, 0.5, 0.001, 0.001, 255, 255, 255, 255)
 		end
 	end
